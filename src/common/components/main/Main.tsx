196e912ec7a263c72a6f7f18da25d2dad31dcbfc
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-14 02:27:41 +0300

    import LeftSidebar.tsx, Feeds.tsx, RightSidebar.tsx components to Main component. added import styles variable

--- src/common/components/main/Main.tsx
+++ src/common/components/main/Main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import s from './Main.module.css'
+import {LeftSidebar} from "common/components/leftSidebar/LeftSidebar.tsx";
+import {Feeds} from "common/components/feeds/Feeds.tsx";
+import {RightSidebar} from "common/components/rightSidebar/RightSidebar.tsx";
 
 export const Main: React.FC = () => {
     return (
-        <div>
-
+        <div className={s.mainWrapper}>
+            <LeftSidebar/>
+            <Feeds/>
+            <RightSidebar/>
         </div>
     );
 };
