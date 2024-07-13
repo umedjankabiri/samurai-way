fac4b02cde1bb1e7214f208c3407bc975a303e8b
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-14 00:53:30 +0300

    tests is checked, they are working, deleted simple tests

--- src/common/components/__tests__/addition.test.ts
+++
@@ -1,9 +0,0 @@
-import {addition} from "common/components/__tests__/addition.ts";
-
-test("result should be correct", ()=> {
-    const a: number = 10;
-    const b: number = 20;
-    const result = addition(a, b);
-
-    expect(result).toBe(30);
-})
--- src/common/components/__tests__/addition.ts
+++
@@ -1,3 +0,0 @@
-export const addition = (a: number, b: number)=> {
-    return a + b;
-}
