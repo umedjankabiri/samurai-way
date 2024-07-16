d2f3da432cfa49f977d8b0ec82554af453973f0f
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-17 00:03:23 +0300

    added Title.tsx component

--- src/common/components/feeds/Feeds.tsx
+++ src/common/components/feeds/Feeds.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import s from './Feeds.module.css'
 import {Stories} from "common/components/feeds/Stories/Stories.tsx";
+import s1 from "common/components/title/Title.module.css";
+import {Title} from "common/components/title/Title.tsx";
 
 export const Feeds: React.FC = () => {
     return (
         <section className={s.feeds}>
+            <Title className={s1.storiesTitle}>Stories</Title>
             <Stories/>
         </section>
     );
