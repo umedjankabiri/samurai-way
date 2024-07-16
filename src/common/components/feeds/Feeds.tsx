4e1703bc2fb9af0302f67540ed2c38d129091135
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-16 23:25:30 +0300

    added Stories.tsx component

--- src/common/components/feeds/Feeds.tsx
+++ src/common/components/feeds/Feeds.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import s from './Feeds.module.css'
+import {Stories} from "common/components/feeds/Stories/Stories.tsx";
 
 export const Feeds: React.FC = () => {
     return (
         <section className={s.feeds}>
-            
+            <Stories/>
         </section>
     );
 };
