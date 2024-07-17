efe61451506ef819c56020e8862ae5dfaf9549ad
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-17 18:58:06 +0300

    created PostBody.tsx component

---
+++ src/common/components/feeds/Post/postBody/PostBody.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from "common/components/feeds/Post/postBody/PostBody.module.css"
+
+export const PostBody: React.FC = () => {
+    return (
+        <div className={s.postBody}>
+            <div className={s.postBodyWrapper}>
+
+            </div>
+        </div>
+    );
+};
