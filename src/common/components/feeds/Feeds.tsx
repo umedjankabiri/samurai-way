bb1f22085a8d3ec078b9fd11cfa72eec113b6985
umedjan kabiri <umedjan.kabiri@gmail.com>
2024-07-17 18:53:40 +0300

    Updated Feeds.tsx component added new components

--- src/common/components/feeds/Feeds.tsx
+++ src/common/components/feeds/Feeds.tsx
@@ -3,6 +3,9 @@ import s from './Feeds.module.css'
 import {Stories} from "common/components/feeds/Stories/Stories.tsx";
 import s1 from "common/components/title/Title.module.css";
 import {Title} from "common/components/title/Title.tsx";
+import {PostHeader} from "common/components/feeds/Post/postHeader/PostHeader.tsx";
+import {PostBody} from "common/components/feeds/Post/postBody/PostBody.tsx";
+import {PostFooter} from "common/components/feeds/Post/postFooter/PostFooter.tsx";
 
 export const Feeds: React.FC = () => {
     return (
@@ -14,6 +17,14 @@ export const Feeds: React.FC = () => {
                 <Stories/>
                 <Stories/>
                 <Stories/>
+            </div>
+            <div className={s.titleWrapper}>
+                <Title className={s1.storiesTitle}>Feeds</Title>
+            </div>
+            <div className={s.componentWrapper}>
+                <PostHeader/>
+                <PostBody/>
+                <PostFooter/>
             </div>
         </section>
     );
